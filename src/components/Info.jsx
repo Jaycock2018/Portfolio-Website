980250dda4528af1eb1f6537939cde484456e799
Jaycock2018 <jayvan.aycock@gmail.com>
2023-04-29 13:23:56 -0500

    Deleted picture and fixed the about me section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import doggies from '../assets/doggies.jpg';
 
 const Info = () => {
   return (
-    <div id='info' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 flex flex-col md:flex-row bg-gray-300 rounded-br-full rounded-bl-full'>
-      <div className='flex-1 p-4 md:p-20 md:w-2/3'>
+    <div id='info' className='max-w-[1040px] m-auto pr-12 p-4 py-16 md:flex md:justify-center md:pl-20 bg-gray-300 rounded-br-full rounded-bl-full'>
+      <div className='flex-1 p-10 md:p-20 md:w-2/3 '>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>About Me</h1>
         <p className='text-center py-8'>
           Hello, I'm Jayvan Aycock, a frontend web developer with a strong foundation in management information systems. I'm passionate about creating visually stunning websites that engage and captivate audiences.
@@ -51,14 +50,6 @@ const Info = () => {
             Microsoft Azure
           </li>
         </ul>
-      </div>
-      <div className='flex-1 p-4 md:p-10 md:w-1/3 md:text-center md:flex items-center justify-center'>
-      <img
-            src={doggies}
-            alt='My Dogs, Emma and Ivy'
-            className='hidden md:block object-contain h-full w-full md:max-w-none md:w-full transition-transform duration-300 ease-in-out hover:scale-110'
-            style={{ maxWidth: "300px", width: "100%" }}
-      />
       </div>
     </div>
   );
