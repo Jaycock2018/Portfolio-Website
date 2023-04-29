9913d0b7bdd7d5814a9af7c4d81d609526d78d42
Jaycock2018 <jayvan.aycock@gmail.com>
2023-04-29 09:56:48 -0500

    Updated code and added information

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,17 +7,18 @@ const Info = () => {
       <div className='flex-1 p-4 md:p-20 md:w-2/3'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>About Me</h1>
         <p className='text-center py-8'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, saepe
-          voluptas expedita cumque itaque, accusantium provident ad tempora
-          obcaecati, laudantium quis nobis. Corporis iusto fugiat magni esse
-          sapiente dolor ratione!
+          Hello, I'm Jayvan Aycock, a frontend web developer with a strong foundation in management information systems. I'm passionate about creating visually stunning websites that engage and captivate audiences.
         </p>
-        <h2 className='text-4xl font-bold text-center text-[#001b5e]'>Skills</h2>
+        <p className='text-center bg-[#001b5e]/25 p-4 rounded-md'>
+          While frontend development is my primary focus, I'm eager to learn more about backend development, mobile, and gaming. I'm committed to delivering exceptional results, working closely with clients to bring their vision to life with precision and artistry.
+        </p>
+        <p className='text-center py-8'>
+          If you're looking for a dedicated frontend developer who's eager to learn and grow, I would be thrilled to work with you. Let's create something amazing together!
+        </p>
+        <h2 className='text-4xl font-bold text-center text-[#001b5e]'>Fun Facts</h2>
         <ul className='text-center py-8'>
           <li className='inline-block rounded-full py-2 px-4 bg-gray-200 text-gray-800 hover:bg-[#001b5e] hover:text-white transition duration-300 ease-in-out'>JavaScript</li>
-          <li className='inline-block rounded-full py-2 px-4 bg-gray-200 text-gray-800 hover:bg-[#001b5e] hover:text-white transition duration-300 ease-in-out'>React</li>
-          <li className='inline-block rounded-full py-2 px-4 bg-gray-200 text-gray-800 hover:bg-[#001b5e] hover:text-white transition duration-300 ease-in-out'>CSS</li>
-          <li className='inline-block rounded-full py-2 px-4 bg-gray-200 text-gray-800 hover:bg-[#001b5e] hover:text-white transition duration-300 ease-in-out'>HTML</li>
+          
         </ul>
       </div>
       <div className='flex-1 p-4 md:p-10 md:w-1/3 md:text-center md:flex items-center justify-center'>
